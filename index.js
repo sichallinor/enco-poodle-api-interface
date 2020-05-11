2eb3621773e0eeb7cec1e992164d60fa85d91f69
sichallinor <sdchallinor@gmail.com>
2020-05-11 11:28:14 +0100

    Updates for v0.1.9

--- index.js
+++ index.js
@@ -1,40 +1,336 @@
 'use strict';
 
-var poodleCrudInterface = require('enco-poodle-crud-interface');
-var poodleDavInterface = require('enco-poodle-dav-interface');
+//var poodleCrudInterface = require('enco-poodle-crud-interface');
+//var poodleCrudInterface = require('../REPO_TDR_ENCO_POODLE_CRUD_INTERFACE/index.js');
+//import poodleCrudInterface from '../REPO_TDR_ENCO_POODLE_CRUD_INTERFACE/index';
+import poodleCrudInterface from 'enco-poodle-crud-interface';
+
+//var poodleDavInterface = require('enco-poodle-dav-interface');
+//var poodleDavInterface = require('../REPO_TDR_ENCO_POODLE_DAV_INTERFACE/index.js');
+//import poodleDavInterface from '../REPO_TDR_ENCO_POODLE_DAV_INTERFACE/index';
+import poodleDavInterface from 'enco-poodle-dav-interface';
+
+//var poodleStoreInterface = require('');
+//var poodleStoreInterface = require('./services/ApiService_LocalStorage.js');
+import poodleStoreInterface from './services/ApiService_LocalStorage';
 
 
-module.exports =  {
+
+//module.exports =  {
+export default {
+
+//================================================================================
+
+    // NEW V2 CLEAN FUNCTIONS (SPECIFIC INTERFACE LOGIC IS DELEGATED TO THE INTERFACE SERVICE)
+
+    // NOTE : KEEP IT SIMPLE ... NOT COMPLETE IN THIS LAYER YET !!!!!!
+    // NOTE : PURPOSE OF THIS LAYER IS 
+    //        1) TO ACT AS A SINGLE INTERFACE FOR MULTIPLE APIS 
+    //        2) FORWARD MODES AS INPUT TO THE RESPECTIVE APIS
+    //        3) AGGREATE DATA FROM DIFFERENT APIS 
+    //
+    // NOTE : THIS CLASS UNDERSTANDS APIS AND WHAT TO SEND - BUT IT DOES NOT UNDERSTAND ANY SERVICE SPECIFIC LOGIC
+    // NOTE : THE SERVICE CLASSES UNDERSTAND THEIR OWN LOGIC - BUT THEY DO NOT UNDERSTAND MODE  
+
+//================================================================================
+
+
+
 
     getInterface(mode){
-        if(mode.apitype==="phpcrud"){
+        if(mode.apitype==="localstorage"){
+            // LOCAL STORAGE
+            return poodleStoreInterface;
+        }else if(mode.apitype==="phpcrud" || mode.apitype==="labrador"){
             return poodleCrudInterface;
         }else if(mode.apitype==="caldav" || mode.apitype==="carddav"){
             return poodleDavInterface;
         }
+        console.log("ERROR : NO APITYPE FOUND")
+        return null;
+    },
+
+
+    authenticateWithEmailPassword(mode){
+        var interF = this.getInterface(mode);
+        if(interF) return interF.authenticateWithEmailPassword(mode);
+        return null;
     },
 
 
     getItems(mode=null) {
+        var self = this;
+        var promises = [];
+
+        mode['is_reading']=true;
+
+        if( mode.mode_type==='items_aggregator' && mode.hasOwnProperty('modes') ){
+            // ---------------------------------------
+            // get a promise from each child mode
+            for(var i=0; i<mode.modes.length; i++){
+                var childMode = mode.modes[i];
+                var prom = this.getItems(childMode);
+                if(prom) promises.push(prom);
+            }
+        }
+        // ---------------------------------------
+        // and get a promise from this parent mode
         try{
-            return this.getInterface(mode).getItems(mode);
+            if(mode.mode_type==='items' || !mode.hasOwnProperty('mode_type')){
+                console.log("items...")
+                var prom = null;
+                var interF = this.getInterface(mode);
+                if(interF) prom = interF.getItems(mode);
+                if(prom) promises.push(prom);
+            }
         }catch(err){
             console.log("ERROR : ",err);
-            resolve();
+            mode['is_reading']=false;
+            return null;
         }
+        
+        if(promises.length>0){
+            // ---------------------------------------
+            // execute all promises
+            return new Promise(function(resolve, reject) {
+                Promise.all(promises).then(function(values) {
+
+                    // AGREGATOR ... 
+                    if( mode.mode_type==='items_aggregator' && mode.hasOwnProperty('modes') ){
+                        mode.aggregateChildrensData();
+                    }
+
+                    mode['is_reading']=false;
+
+                    if(values.length===1) {
+                        resolve(values[0]) // resolve with the result of a single promise
+                    }else{
+                        resolve(values); // resolve with an array of all the results from all the promises
+                    }
+       
+                }).catch(error => { 
+                    if(error && error.message){
+                        console.log("API ERROR GETTING DATA : ",error.message)
+                    }else{
+                        console.log("API ERROR GETTING DATA")
+                    }
+                  mode['is_reading']=false;
+                  reject();
+                });
+            });
+        }else{
+            mode['is_reading']=false;
+            return null;
+        }
+
     },
+
 
     getItem(mode=null) {
-        return this.getInterface(mode).getItem(mode);
+        //return this.getInterface(mode).getItem(mode);
+
+        var self = this;
+        var promises = [];
+
+        mode['is_reading']=true;
+
+        if( mode.hasOwnProperty('modes') ){
+            // ---------------------------------------
+            // get a promise from each child mode
+            for(var i=0; i<mode.modes.length; i++){
+                var childMode = mode.modes[i];
+                var prom = this.getItem(childMode);
+                if(prom) promises.push(prom);
+            }
+        }
+        // ---------------------------------------
+        // and get a promise from this parent mode
+        try{
+            if(mode.mode_type==='model' || !mode.hasOwnProperty('mode_type') ){
+                var prom = null;
+                var interF = this.getInterface(mode);
+                if(interF) prom = interF.getItem(mode);
+                if(prom) promises.push(prom);
+            }
+        }catch(err){
+            console.log("ERROR : ",err);
+            mode['is_reading']=false;
+            return null;
+        }
+        
+        if(promises.length>0){
+            // ---------------------------------------
+            // execute all promises
+            return new Promise(function(resolve, reject) {
+                Promise.all(promises).then(function(values) {
+                    //self.aggregateChildrensData(mode);
+                    mode['is_reading']=false;
+                    resolve(values);        
+                }).catch(error => { 
+                  console.error(error.message)
+                  mode['is_reading']=false;
+                  reject();
+                });
+            });
+        }else{
+            mode['is_reading']=false;
+            return null;
+        }
+
     },
 
+
+
+    // UPDATE OF THE PRIMARY MODEL
     updateItem(mode=null) {
-        return this.getInterface(mode).updateItem(mode);
+        console.log("updateItem (API) : starting for mode_ref : ",mode.reference);
+        //return this.getInterface(mode).updateItems(mode);
+        
+        var self = this;
+        var promises = [];
+        /*
+        if( mode.hasOwnProperty('modes') ){
+            // ---------------------------------------
+            // get a promise from each child mode
+            for(var i=0; i<mode.modes.length; i++){
+                var childMode = mode.modes[i];
+                var prom = this.updateItem(childMode);
+                if(prom) promises.push(prom);
+            }
+        
+        */
+        // ---------------------------------------
+        // and get a promise from this parent mode
+        try{
+            if(mode.mode_type==='items' || !mode.hasOwnProperty('mode_type')){
+                var prom = null;
+                var interF = this.getInterface(mode);
+                if(interF) prom = interF.updateItem(mode);
+                // IF A PROMISE HAS BEEN RETURNED ... ADD IT
+                // HOWEVER THE updateItems FUNCTION MAY RETURN NULL (nothing to do)
+                if(prom) promises.push(prom);
+            }
+        }catch(err){
+            console.log("ERROR : UPDATING ",err.message,mode);
+            //throw "ERROR : UPDATING ITEMS";
+            //return null
+        }
+        
+        if(promises.length>0){
+            // ---------------------------------------
+            // execute all promises
+            return new Promise(function(resolve, reject) {
+                Promise.all(promises).then(function(values) {
+                    resolve(values);
+                }).catch(error => { 
+                  console.error(error.message)
+                  reject();
+                });
+            });
+        }else{
+            return null;
+        }
+
+    },
+    // FOR BULK UPDATES (NORMALLY STORED IN "models_bulk")
+    updateItems(mode=null) {
+        //return this.getInterface(mode).updateItems(mode);
+        var self = this;
+        var promises = [];
+        if( mode.hasOwnProperty('modes') ){
+            // ---------------------------------------
+            // get a promise from each child mode
+            for(var i=0; i<mode.modes.length; i++){
+                var childMode = mode.modes[i];
+                var prom = this.updateItems(childMode);
+                if(prom) promises.push(prom);
+            }
+        }
+        // ---------------------------------------
+        // and get a promise from this parent mode
+        try{
+            if(mode.mode_type==='items' || !mode.hasOwnProperty('mode_type')){
+                var prom = null;
+                var interF = this.getInterface(mode);
+                if(interF) prom = interF.updateItems(mode);
+                // IF A PROMISE HAS BEEN RETURNED ... ADD IT
+                // HOWEVER THE updateItems FUNCTION MAY RETURN NULL (nothing to do)
+                if(prom) promises.push(prom);
+            }
+        }catch(err){
+            console.log("ERROR : UPDATING ",err.message,mode);
+            //throw "ERROR : UPDATING ITEMS";
+            //return null
+        }
+        
+        if(promises.length>0){
+            // ---------------------------------------
+            // execute all promises
+            return new Promise(function(resolve, reject) {
+                Promise.all(promises).then(function(values) {
+                    resolve(values);
+                }).catch(error => { 
+                  console.error(error.message)
+                  reject();
+                });
+            });
+        }else{
+            return null;
+        }
+
     },
 
     createItem(mode=null) {
         return this.getInterface(mode).createItem(mode);
     },
+    // FOR BULK UPDATES (NORMALLY STORED IN "models_bulk")
+    createItems(mode=null) {
+        //return this.getInterface(mode).updateItems(mode);
+        var self = this;
+        var promises = [];
+        if( mode.hasOwnProperty('modes') ){
+            // ---------------------------------------
+            // get a promise from each child mode
+            for(var i=0; i<mode.modes.length; i++){
+                var childMode = mode.modes[i];
+                var prom = this.createItems(childMode);
+                if(prom) promises.push(prom);
+            }
+        }
+        // ---------------------------------------
+        // and get a promise from this parent mode
+        try{
+            if(mode.mode_type==='items' || !mode.hasOwnProperty('mode_type')){
+                var prom = null;
+                var interF = this.getInterface(mode);
+                if(interF) prom = interF.createItems(mode);
+                // IF A PROMISE HAS BEEN RETURNED ... ADD IT
+                // HOWEVER THE updateItems FUNCTION MAY RETURN NULL (nothing to do)
+                if(prom) promises.push(prom);
+            }
+        }catch(err){
+            console.log("ERROR : CREATING ",err.message,mode);
+            //throw "ERROR : UPDATING ITEMS";
+            //return null
+        }
+        
+        if(promises.length>0){
+            // ---------------------------------------
+            // execute all promises
+            return new Promise(function(resolve, reject) {
+                Promise.all(promises).then(function(values) {
+                    resolve(values);
+                }).catch(error => { 
+                  console.error(error.message)
+                  reject();
+                });
+            });
+        }else{
+            return null;
+        }
+
+    },
+
 
     deleteItem(mode=null) {
         return this.getInterface(mode).deleteItem(mode);
---
+++ services/ApiService_LocalStorage.js
@@ -0,0 +1,238 @@
+'use strict';
+
+
+//----------------
+// POODLE-MODE IN DEV MODE
+//var Mode = require('enco-poodle-mode');
+//var Mode = require('../../REPO_TDR_ENCO_POODLE_MODE/index.js');
+import { Mode } from '../../REPO_TDR_ENCO_POODLE_MODE/index';
+
+//----------------
+// POODLE-UTILS IN DEV MODE
+//var poodleUtils = require('enco-poodle-utils');
+//var poodleUtils = require('../../REPO_TDR_ENCO_POODLE_UTILS/index.js');
+import { poodleUtils } from '../../REPO_TDR_ENCO_POODLE_UTILS/index';
+
+
+
+//module.exports =  {
+export default {
+
+
+
+    debug : true,
+
+    mode : null,
+
+    default_mode : { 
+                    // API MODE PROPERTIES
+                    apitype : null,
+                    urlbase : null,
+                    urlpath : null,
+                    port : 80,
+
+                    // REQUEST
+                    search_phrase : null,
+                    context : null,
+                    identity : null,
+
+                    // RESULT : ITEMS
+                    items : [],
+                    // RESULT : MODEL
+                    models : [],
+
+                    schemas : []
+                },
+
+
+    alterMode(mode){
+        if(this.mode){
+            this.mode = Object.assign(this.mode, mode);
+        }else{
+            this.mode = mode;
+        }
+        if(this.debug) console.log("MODE ALTERED TO : ", this.mode);
+        return this.mode;
+    },
+    setMode(mode){
+        this.mode = mode;
+        if(this.debug) console.log("MODE SET AS : ", this.mode);
+    },	
+    getMode(){
+    	return this.mode;
+    },
+
+    requireModePropertiesOrError(arr){
+        if(this.debug)  console.log("requireModePropertiesOrError");
+    	var mode = this.getMode();
+    	var missing = [];
+    	for(var i=0; i<arr.length; i++){
+    		if(!mode.hasOwnProperty(arr[i]) || mode[arr[i]]===null ){
+    			missing.push(arr[i]);
+    		}
+    	}
+    	if(missing.length>0){
+            //throw "ERROR : MODE IS MISSING PROPS : " + missing;
+    		console.log("ERROR : MODE IS MISSING PROPS : ",missing);
+            //throw "ERROR : MODE IS MISSING PROPS : " + missing;
+    		return false
+    	}
+
+
+    	return true;
+    },
+
+    getItems(mode=null) {
+        var self = this;
+    	if(mode) this.setMode(mode)
+    	mode = this.getMode();
+    	//------------------------
+
+        var destiation_arr = mode.items;
+
+        // STORE REFERENCES
+        //var schema = (mode.schemas && mode.schemas.length>0) ? mode.schemas[0] : null;
+
+        if(this.debug)  console.log("getItems");
+
+
+        return new Promise(function(resolve, reject) {
+
+            destiation_arr.length = 0; // TO EMPTY THE ARRAY
+
+            // LOOP EVERYTHING IN LOCALSTORAGE AND PLACE INTO THE ITEMS
+            for (var prop in localStorage){
+                var jstr = localStorage.getItem(prop);
+                try{
+                    var jobj = JSON.parse(jstr)
+                    //set as not DIRTY (saved)
+                    self.setCompleteHierarchyNotDirty(jobj)
+
+                    destiation_arr.push(jobj);
+                }catch(ex){
+                }
+            }
+
+            console.log("getItems : RESULT : ",destiation_arr.length)
+
+            resolve();
+
+        });
+
+
+    },
+
+    getItem(mode=null) {
+
+    },
+
+    updateItem(mode=null) {
+        console.log("updateItem (LOCAL) : starting for mode_ref : ",mode.reference);
+        return this.storeItem(mode);
+    },
+    updateItems(mode=null) {
+
+    },
+
+    createItem(mode=null) {
+        console.log("createItem : starting for mode_ref : ",mode.reference);
+        return this.storeItem(mode);
+    },
+
+    storeItem(mode=null){
+        var self = this;
+        if(mode) this.setMode(mode)
+        mode = this.getMode();
+        //------------------------
+        if(!mode.models || mode.models.length==0) {
+            console.log("createItem : no models");
+            return null;
+        }
+        var outgoingModel = mode.models[0];
+
+        return new Promise(function(resolve, reject) {
+            console.log("createItem : (promise) starting");
+            try{
+                var id = ""
+                if(outgoingModel.hasOwnProperty('id')){
+                    id = outgoingModel.id;
+                }else{
+                    id = poodleUtils.generateUID();
+                    outgoingModel['id'] = id;
+                }
+                var outgoingModelStr = JSON.stringify(outgoingModel);
+                console.log("createItem : string to store : ",outgoingModelStr)
+                localStorage.setItem(id,outgoingModelStr);
+                
+                //set as not DIRTY (saved)
+                //outgoingModel._dirty = false;
+                self.setCompleteHierarchyNotDirty(outgoingModel)
+                //----
+
+
+                console.log("createItem : (promise) resolving");
+                resolve();
+            }catch(err){
+                console.log("createItem : (promise) rejecting",err);
+                reject();
+            }
+
+        }); 
+    },
+
+    
+    setCompleteHierarchyNotDirty(obj){
+        if(obj.hasOwnProperty("_dirty")) obj._dirty = false;
+
+        for(var key in obj) {
+            var child = obj[key];
+            if(Array.isArray(child)){
+                for(var i=0; i<child.length; i++){
+                    var childItem = child[i];
+                    if(typeof childItem === 'object' && childItem !== null){
+                        this.setCompleteHierarchyNotDirty(childItem)
+                    }
+                }
+            }else if(typeof child === 'object' && child !== null){
+                this.setCompleteHierarchyNotDirty(child)
+            }
+        }
+    },
+    
+
+    deleteItem(mode=null) {
+        if(mode) this.setMode(mode)
+        mode = this.getMode();
+        //------------------------
+        if(!mode.models || mode.models.length==0) {
+            console.log("deleteItem : no models");
+            return null;
+        }
+        var outgoingModel = mode.models[0];
+
+        if(this.debug)  console.log("deleteItem");
+        
+
+        return new Promise(function(resolve, reject) {
+            console.log("deleteItem : (promise) starting");
+            try{
+                var id = ""
+                if(outgoingModel.hasOwnProperty('id')){
+                    id = outgoingModel.id;
+
+                    localStorage.removeItem(id)
+                    console.log("deleteItem : (promise) resolving");
+                    resolve();
+                }else{
+                    reject();
+                }
+            }catch(err){
+                console.log("deleteItem : (promise) rejecting",err);
+                reject();
+            }
+            //---------------------------------------------
+        });
+    },
+
+
+}
